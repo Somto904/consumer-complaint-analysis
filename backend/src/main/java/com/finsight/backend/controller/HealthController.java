a77dc219d815663b4c76f0b77b47ddc490052677
Somto904 <somtoezenagu@gmail.com>
2026-08-16 15:29:14 -0400

    Update HealthController.java

--- backend/src/main/java/com/finsight/backend/controller/HealthController.java
+++ backend/src/main/java/com/finsight/backend/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.finsight.backend;
+package com.finsight.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
